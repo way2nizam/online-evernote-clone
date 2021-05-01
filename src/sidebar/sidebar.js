06d100b4f56f201d02cfb14d119e693936d86435
nizamy710 <nizamy710@gamil.com>
2021-05-01 23:11:40 +0530

    fix: modules

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "../sidebar/styles";
-import List from "@material-ui/core/List";
+// import List from "@material-ui/core/List";
 import { Divider, Button } from "@material-ui/core";
 import SidebarItemComponent from "../sidebaritem/sidebaritem";
 
